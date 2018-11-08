12f30075e628253dd8cd715159b04edfb61b240c
Ryan Coates <ryan.coates@gmail.com>
2018-11-07 20:48:34 -0700

    question controller work

--- StackUnderflowRDC.Business/QuestionService.cs
+++ StackUnderflowRDC.Business/QuestionService.cs
@@ -1,26 +1,61 @@
 ﻿using StackUnderflowRDC.Data;
 using StackUnderflowRDC.Entities;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StackUnderflowRDC.Business
 {
     public class QuestionService
     {
-        private readonly ApplicationDbContext _ctx;
+        private readonly DataContext _ctx;
 
-        public QuestionService(ApplicationDbContext ctx)
+        public QuestionService(DataContext ctx)
         {
             _ctx = ctx;
         }
 
-        public Question NewQuestion()
+        public List<Question> GetAllQuestions()
         {
-         
+            return _ctx.Questions.ToList();
         }
 
-        public Question CloseQuestion(Question q, Response r)
+        public Question GetQuestionById(int id)
         {
-      
+            return _ctx.Questions.Find(id);
+        }
+
+        public Question GetQuestionByResponseId(int id)
+        {
+            return _ctx.Questions.Where(q => q.AnswerId == id).First();
+        }
+
+        public Question NewQuestion(QuestionForCreation data)
+        {
+            Question q = new Question();
+            q.PostedAt = new DateTimeOffset();
+            q.Score = 0;
+            q.Answered = false;
+            q.Author = data.Author;
+            q.Body = data.Body;
+            q.Comments = new HashSet<Comment>();
+            q.Responses = new HashSet<Response>();
+
+
+            _ctx.Questions.Add(q);
+            _ctx.SaveChanges();
+
+            return q;
+        }
+
+        public void CloseQuestion(Response r)
+        {
+            var q = GetQuestionByResponseId(r.Id);
+            q.Answered = true;
+            q.AnswerId = r.Id;
+            r.isAnswer = true;
+
+
         }
     }
 }
